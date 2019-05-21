80ff49134c32ac19384efbfc71c834ebd0ab4878
Administrator <Administrator@PC-201905201005>
2019-05-21 17:02:56 +0800

    第一次

---
+++ src/main/java/com/example/demo/MybatisPlugApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisPlugApplication.class, args);
+	}
+}
